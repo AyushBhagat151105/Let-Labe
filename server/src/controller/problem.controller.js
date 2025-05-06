32597df21c81c4d702e9ffff3dd29c497115d51c
Ayush_Bhagat <103896902+AyushBhagat151105@users.noreply.github.com>
2025-05-06 23:20:38 +0530

    refactor: Enhance createProblem function and improve error handling in problem management

--- server/src/controller/problem.controller.js
+++ server/src/controller/problem.controller.js
@@ -8,6 +8,41 @@ import { ApiError } from "../utils/apiErrors.js";
 import { ApiResponse } from "../utils/apiResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 
+const checkRefrenceSolution = async (referenceSolution, testcases) => {
+  for (const [language, solutionCode] of Object.entries(referenceSolution)) {
+    const languageId = getJudge0LanguageId(language);
+
+    if (!languageId)
+      throw new ApiError(400, `${language} Language is not supported`);
+
+    const submissions = testcases.map(({ input, output }) => {
+      return {
+        source_code: solutionCode,
+        language_id: languageId,
+        stdin: input,
+        expected_output: output,
+      };
+    });
+
+    const submissionResults = await submitBatch(submissions);
+
+    const tokens = submissionResults.map((res) => res.token);
+
+    const results = await pollBatchResults(tokens);
+
+    for (let i = 0; i < results.length; i++) {
+      const result = results[i];
+
+      if (result.status.id !== 3) {
+        throw new ApiError(
+          400,
+          `Testcase ${i + 1} failed for language ${language}`
+        );
+      }
+    }
+  }
+};
+
 export const createProblem = asyncHandler(async (req, res) => {
   const {
     title,
@@ -21,42 +56,7 @@ export const createProblem = asyncHandler(async (req, res) => {
     referenceSolutions,
   } = req.body;
 
-  for (const [language, solutionCode] of Object.entries(referenceSolutions)) {
-    const languageId = getJudge0LanguageId(language);
-
-    if (!languageId) {
-      throw new ApiError(400, `Language ${language} is not supported`);
-    }
-
-    const submissions = testcases.map(({ input, output }) => ({
-      source_code: solutionCode,
-      language_id: languageId,
-      stdin: input,
-      expected_output: output,
-    }));
-
-    const submissionResults = await submitBatch(submissions);
-
-    const tokens = submissionResults.map((res) => res.token);
-
-    const results = await pollBatchResults(tokens);
-    console.log("Contoler results log:-  ", results);
-
-    for (let i = 0; i < results.length; i++) {
-      const result = results[i];
-
-      console.log("-----------Result--------------", result.status?.id);
-
-      if (result.status?.id !== 3) {
-        throw new ApiError(
-          400,
-          `Testcase ${i + 1} failed for language ${language}`
-        );
-      }
-    }
-  }
-
-  console.log(req.user_id);
+  checkRefrenceSolution(referenceSolutions, testcases);
 
   const Problem = await prisma.problem.create({
     data: {
@@ -78,19 +78,95 @@ export const createProblem = asyncHandler(async (req, res) => {
 
   return res.status(201).json(
     new ApiResponse(201, "New Problem created sussesfull", {
-      data: {
-        Problem,
-      },
+      Problem,
     })
   );
 });
 
-export const getAllProblems = asyncHandler(async (req, res) => {});
+export const getAllProblems = asyncHandler(async (req, res) => {
+  const problems = await prisma.problem.findMany();
 
-export const getProblemsById = asyncHandler(async (req, res) => {});
+  if (!problems) throw new ApiError(400, "Failed to get Problems");
 
-export const updateProblem = asyncHandler(async (req, res) => {});
+  return res
+    .status(200)
+    .json(new ApiResponse(200, "Problems Found", { problems }));
+});
 
-export const deleteProblem = asyncHandler(async (req, res) => {});
+export const getProblemsById = asyncHandler(async (req, res) => {
+  const { id } = req.params;
+
+  const problem = await prisma.problem.findUnique({
+    where: {
+      id: id,
+    },
+  });
+
+  if (!problem) throw new ApiError(400, "May be Id is wrong");
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, "Problem Find", { problem }));
+});
+
+export const updateProblem = asyncHandler(async (req, res) => {
+  const {
+    title,
+    description,
+    difficulty,
+    tags,
+    examples,
+    constraints,
+    testcases,
+    codeSnippets,
+    referenceSolutions,
+  } = req.body;
+  const { id } = req.params;
+
+  checkRefrenceSolution(referenceSolutions, testcases);
+
+  const Problem = await prisma.problem.update({
+    where: {
+      id: id,
+    },
+    data: {
+      title,
+      description,
+      difficulty,
+      tags,
+      examples,
+      constraints,
+      testcases,
+      codeSnippets,
+      referenceSolutions,
+      userId: req.user_id,
+    },
+  });
+  if (!Problem) {
+    throw new ApiError(400, "Failed To create Probem");
+  }
+
+  return res.status(201).json(
+    new ApiResponse(201, "New Problem created sussesfull", {
+      Problem,
+    })
+  );
+});
+
+export const deleteProblem = asyncHandler(async (req, res) => {
+  const { id } = req.params;
+
+  if (!id) throw new ApiError(400, "Give id");
+
+  const deleteProblem = await prisma.problem.delete({
+    where: {
+      id: id,
+    },
+  });
+
+  if (!deleteProblem) throw new ApiError(404, "Not found");
+
+  return res.status(200).json(new ApiResponse(200, "Problem deleted"));
+});
 
 export const getAllProblemsSolvedByUser = asyncHandler(async (req, res) => {});
