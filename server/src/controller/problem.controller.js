e0cf5e8b6648fd6ff07bf6fdd768df9ed45558f9
Ayush_Bhagat <103896902+AyushBhagat151105@users.noreply.github.com>
2025-05-06 12:08:01 +0530

    feat: Refactor createProblem function to improve error handling and code structure

--- server/src/controller/problem.controller.js
+++ server/src/controller/problem.controller.js
@@ -1,11 +1,12 @@
+import { prisma } from "../client/index.js";
 import {
   getJudge0LanguageId,
   pollBatchResults,
   submitBatch,
 } from "../lib/judge0.lib.js";
 import { ApiError } from "../utils/apiErrors.js";
+import { ApiResponse } from "../utils/apiResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
-import { Prisma } from "@prisma/client";
 
 export const createProblem = asyncHandler(async (req, res) => {
   const {
@@ -24,30 +25,63 @@ export const createProblem = asyncHandler(async (req, res) => {
     throw new ApiError(403, "You are not allowed to create problems");
   }
 
-  try {
-    for (const [language, solutionCode] of Object.entries(referenceSolutions)) {
-      const languageId = getJudge0LanguageId(language);
+  for (const [language, solutionCode] of Object.entries(referenceSolutions)) {
+    const languageId = getJudge0LanguageId(language);
 
-      if (!languageId) {
-        throw new ApiError(400, `Language ${language} is not supported`);
-      }
-
-      const submissions = testcases.map(({ input, output }) => ({
-        source_code: solutionCode,
-        language_id: languageId,
-        stdin: input,
-        expected_output: output,
-      }));
-
-      const submissionResults = await submitBatch(submissions);
-
-      const tokens = submissionResults.map((res) => res.token);
-
-      const results = await pollBatchResults(tokens);
+    if (!languageId) {
+      throw new ApiError(400, `Language ${language} is not supported`);
     }
-  } catch (error) {
-    console.log("create problems error:- ", error);
-    throw new ApiError(400, "Error in Createing Problem", error);
+
+    const submissions = testcases.map(({ input, output }) => ({
+      source_code: solutionCode,
+      language_id: languageId,
+      stdin: input,
+      expected_output: output,
+    }));
+
+    const submissionResults = await submitBatch(submissions);
+
+    const tokens = submissionResults.map((res) => res.token);
+
+    const results = await pollBatchResults(tokens);
+
+    for (let i = 0; i < results.length; i++) {
+      const result = results[i];
+
+      if (result.status.id !== 3) {
+        throw new ApiError(
+          400,
+          `Testcase ${i + 1} failed for language ${language}`
+        );
+      }
+    }
+
+    // save the problem to the DB
+    const Problem = await prisma.problem.create({
+      data: {
+        title,
+        description,
+        difficulty,
+        tags,
+        examples,
+        constraints,
+        testcases,
+        codeSnippets,
+        referenceSolutions,
+        userId: req.user.id,
+      },
+    });
+    if (!Problem) {
+      throw new ApiError(400, "Failed To create Probem");
+    }
+
+    return res.status(201).json(
+      new ApiResponse(201, "New Problem created sussesfull", {
+        data: {
+          Problem,
+        },
+      })
+    );
   }
 });
 
